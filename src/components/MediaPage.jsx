08d1bad45dce2f48904a4e1aefd4842d963745d0
akimoale <akimoale@fel.cvut.cz>
2025-05-21 00:06:05 +0200

    feat: Udpated MediaPage.jsx for vercel

--- src/components/MediaPage.jsx
+++ src/components/MediaPage.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 const MediaPage = () => {
     return (
         <div className="media-page">
-            <video controls poster="/public/media/poster.jpg">
-                <source src="/public/media/video.mp4" type="video/mp4" />
+            <video controls poster="/media/poster.jpg">
+                <source src="/media/video.mp4" type="video/mp4" />
             </video>
         </div>
     );
